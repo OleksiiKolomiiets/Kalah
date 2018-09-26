a0070e53da9ba016b9fad4c85319690ebe0cdba5
OleksiiKolomiiets <Oleksii.Kolomiiets@onseo.biz>
2018-09-26 09:50:10 +0300

    create a project.

---
+++ Kalah/Kalah/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Kalah
+//
+//  Created by Oleksii  Kolomiiets on 9/26/18.
+//  Copyright © 2018 Oleksii  Kolomiets. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
